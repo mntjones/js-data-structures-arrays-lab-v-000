198fe5a679d11f14d2a7fa8c7a2d98843120cc31
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-12 19:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const newDrivers = [...drivers, name];
+  newDrivers;
+}
+
+function prependDriver(name) {
+  const newDrivers = [name, ...drivers];
+  newDrivers;
+}
+
+function removeLastDriver() {
+  drivers.slice(-1);
+}
+
+function removeFirstDriver() {
+  drivers.slice(1);
+}
